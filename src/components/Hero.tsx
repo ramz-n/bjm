c2c8ce87234c0d7718aae8dcb5f7f92b800ac4c3
ramz-n <aly.ranzam@gmail.com>
2026-07-15 17:39:36 +0545

    add hero compoenent

---
+++ src/components/Hero.tsx
@@ -0,0 +1,13 @@
+const Hero = () => {
+    return (
+        <div className="bg-primary py-20 min-h-100 text-primary-dim px-3">
+            <div className="container mx-auto flex flex-col items-start justify-center gap-8">
+                <p>Narayangarh, Chitwan</p>
+                <h1 className="text-4xl font-bold">A place of worship, at the heart of the community</h1>
+                <p>Welcome to Al Jamiatul Barkatiya Jame Masjid, a place for worship, learning, service and community.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
