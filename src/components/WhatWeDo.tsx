010af3b5afac2135c7297cd35495f1594118019b
ramz-n <aly.ranzam@gmail.com>
2026-07-16 14:55:39 +0545

    update whatwedo component

--- src/components/WhatWeDo.tsx
+++ src/components/WhatWeDo.tsx
@@ -1,6 +1,6 @@
 const WhatWeDo = () => {
     return (
-        <section className="my-5">
+        <section className="my-10">
             <div className="bg-accent container mx-auto rounded-2xl flex flex-col items-center gap-5 py-5">
                 <p className="uppercase text-secondary">Al-Jamiatul Barkatiya Jame Masjid</p>
                 <h3 className="text-5xl text-secondary/50 font-extrabold text-center">Prayer. Learning. Giving.</h3>
