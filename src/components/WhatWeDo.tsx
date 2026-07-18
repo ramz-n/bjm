d71e18d242639cd8c0f3f913c8363f22e86e08e2
ramz-n <aly.ranzam@gmail.com>
2026-07-18 17:38:53 +0545

    add icons

--- src/components/WhatWeDo.tsx
+++ src/components/WhatWeDo.tsx
@@ -1,9 +1,9 @@
 const WhatWeDo = () => {
     return (
         <section className="my-10 px-5">
-            <div className="bg-accent container mx-auto rounded-2xl flex flex-col items-center gap-5 py-5">
-                <p className="uppercase text-accent-dim">Al-Jamiatul Barkatiya Jame Masjid</p>
-                <h3 className="text-5xl text-secondary/50 font-extrabold text-center">Prayer. Learning. Giving.</h3>
+            <div className="bg-accent container mx-auto rounded-2xl flex flex-col items-center gap-5 py-5 p-5">
+                <p className="uppercase text-accent-dim">Al-Jamaetul Barkatiya Jame Masjid</p>
+                <h3 className="text-2xl md:text-5xl text-secondary/50 font-extrabold text-center">Prayer. Learning. Giving.</h3>
                 <p className="text-primary-dim max-w-md text-center text-sm">Our doors are open to worshippers and visitors alike. Come and share in a community rooted in faith and compassion.</p>
             </div>
         </section>
