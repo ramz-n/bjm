49ee118396b21d39ed2be363655840c20d7db615
ramz-n <aly.ranzam@gmail.com>
2026-09-11 17:21:44 +0545

    add favicons and convert the app to pwa

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { registerSW } from "virtual:pwa-register";
 
 import "./index.css";
 import App from "./App.tsx";
 import { LanguageProvider } from "./context/LanguageContext";
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("New contents from Barkati Masjid. Reload?")) {
+      updateSW(true);
+    }
+  },
+  onOfflineReady() {
+    console.log("App is ready to work offline!");
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -13,5 +25,5 @@ createRoot(document.getElementById("root")!).render(
         <App />
       </LanguageProvider>
     </BrowserRouter>
-  </StrictMode>
+  </StrictMode>,
 );
